6d840f2e1c9eaea2bb825339c27a9822320d5906
Joana Cullhaj <joanacullhaj@gmail.com>
2025-11-15 01:54:24 +0100

    Pergatitja e ambjentit

---
+++ my-react-app/src/assets/Footer.jsx
@@ -0,0 +1,11 @@
+//{} => i perdor per te bere insert js
+function Footer(){
+
+    return(
+        <footer>
+            <p>&copy;{new Date().getFullYear()} Your website name</p>
+        </footer>
+    );
+}
+
+export default Footer
---
+++ my-react-app/src/assets/Header.jsx
@@ -0,0 +1,24 @@
+
+function Header(){
+
+    //brenda return mund te vendos HTML te paster
+    //react components mund te kthejne vetem nje element te vetem, por mund te vendos children elements ne te.
+    return (
+        <header>
+            <h1>My website</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Sevices</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+
+
+//perdor export per ta importuar komponentin ne file te tjera
+export default Header
